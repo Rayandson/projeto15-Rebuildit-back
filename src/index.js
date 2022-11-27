692797d651f10ec7eef09873b300b26409673cd3
Rennan <reiony.dev@gmail.com>
2022-11-27 12:31:31 -0300

    refact: unified routes

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import productRoutes from "./routes/productRoutes.js"
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(authRoutes);
-app.use(productRoutes);
-
+app.use(authRoutes, productRoutes);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
