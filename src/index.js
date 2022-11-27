641ee58fbd9b5f90461b25ff06630e55ae15de61
Rennan <reiony.dev@gmail.com>
2022-11-26 23:48:34 -0300

    refact: added use products routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js" 
+import productRoutes from "./routes/productRoutes.js"
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
+app.use(productRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
