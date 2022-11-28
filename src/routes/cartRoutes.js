978fa859c50fe8ea61f5a72b12878b28a8e6dfab
Rennan <reiony.dev@gmail.com>
2022-11-28 11:37:50 -0300

    refact: validateToken

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { InserttoCart } from "../controllers/cart.controller.js";
 import { cartSchemaValidation } from "../middlewares/cartValidation.Middleware.js";
+import { validateToken } from "../middlewares/validateTokenMiddleware.js";
 
 const router = Router()
+
+router.use(validateToken)
 
 router.post("/cart", cartSchemaValidation, InserttoCart);
 
